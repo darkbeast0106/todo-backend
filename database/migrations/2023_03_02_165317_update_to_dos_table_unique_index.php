1583c0e444c41870225f9e18d44d42104694479b
Budaházi Bence <budahazi.bence@petrik.hu>
2023-03-02 18:06:42 +0100

    ToDo tábla, title és user_id együttesen egyedi

---
+++ database/migrations/2023_03_02_165317_update_to_dos_table_unique_index.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('to_dos', function (Blueprint $table) {
+            $table->dropUnique(['title']);
+            $table->unique(['title', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('to_dos', function (Blueprint $table) {
+            $table->dropUnique(['title', 'user_id']);
+            $table->unique(['title']);
+        });
+    }
+};
